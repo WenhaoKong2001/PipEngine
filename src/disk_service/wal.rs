e5e3ab7e9288c175ea79adaa15a454a3a03f291b
mutex <2311566266@qq.com>
2021-07-10 22:30:50 +0800

    wal iterator added.

--- src/disk_service/wal.rs
+++ src/disk_service/wal.rs
@@ -1,12 +1,43 @@
 use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufReader, Read, BufWriter};
+use std::time::{SystemTime, UNIX_EPOCH};
+use crate::mem_table::MemTable;
 
 pub struct WALEntry {
     key: Vec<u8>,
     value: Option<Vec<u8>>,
     timestamp: u128,
     deleted: bool,
+}
+
+pub struct WAL {
+    path: PathBuf,
+    writer: BufWriter<File>,
+}
+
+impl WAL {
+    fn new(dir: PathBuf) -> io::Result<WAL> {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_micros();
+        let path = dir.join(timestamp + ".wal");
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(&path)?;
+        let writer = BufWriter::new(file);
+        Ok(WAL {
+            path,
+            writer,
+        })
+    }
+
+    fn recover(dir:PathBuf)->io::Result<(WAL,MemTable)>{
+        File::open(dir)?;
+    }
 }
 
 pub struct WALIterator {
